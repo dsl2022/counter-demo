f8ec4a9c29c455bb0bfb8330208a7300c2926909
dsl2055 <job2019tmm@gmail.com>
2022-12-21 18:41:37 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -3,9 +3,30 @@ import "./App.css";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const handlerAdd = () => {
+    if (isDisabled) setIsDisabled(false);
+    setCounter(counter + 1);
+  };
+  const handlerSub = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    } else {
+      setIsDisabled(true);
+    }
+    // else
+  };
+  console.log({ counter, isDisabled });
   return (
     <div className="App">
       <h1 className="title">Counter</h1>
+      <div className="counter">{counter}</div>
+      <button onClick={handlerAdd} className="add">
+        Add 1
+      </button>
+      <button disabled={isDisabled} onClick={handlerSub} className="sub">
+        Sub 1
+      </button>
     </div>
   );
 }
